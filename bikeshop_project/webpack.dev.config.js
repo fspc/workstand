8f9d1c366354d61f88f14476c4a9263666f6d122
Drew Larson <drew@drwlrsn.com>
2017-03-04 20:55:06 -0600

    Load data with redux

--- bikeshop_project/webpack.dev.config.js
+++ bikeshop_project/webpack.dev.config.js
@@ -7,8 +7,7 @@ const config = require('./webpack.base.config.js');
 // Use webpack dev server
 config.entry = {
   webpack: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack-dev-server/client?http://workstand.docker:3000',
   ],
   signin: './assets/js/index',
   members: './assets/js/members/index',
@@ -16,7 +15,7 @@ config.entry = {
 };
 
 // override django's STATIC_URL for webpack bundles
-config.output.publicPath = 'http://localhost:3000/assets/bundles/';
+config.output.publicPath = 'http://workstand.docker:3000/assets/bundles/';
 
 config.devtool = 'eval-source-map';
 
